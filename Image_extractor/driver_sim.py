287c1365e6cb5037418583e64a6bcc7e817876a4
Aryan Mangal <amangal@cpn-q07-18.compute.cbls.ccr.buffalo.edu>
2023-02-26 08:15:35 -0500

    Resolve error

--- Image_extractor/driver_sim.py
+++ Image_extractor/driver_sim.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 	runs python scripts to generate raw data
 	and semantic files for each scene
 	"""
-	x_view_path = "../data_collection/x-view"
+	x_view_path = "/user/amangal/projects/aryan/data_collection/x-view"
 	datasets = ["mp3d"]
 	for dataset in datasets:
 		dataset_path = os.path.join(x_view_path, dataset)
@@ -14,10 +14,10 @@ if __name__ == '__main__':
 			scene_path = os.path.join(dataset_path, scene)
 
 			print("python get_scene_objects.py " + dataset + " " + scene)
-			os.system("python get_scene_objects.py " + dataset + " " + scene)
+			os.system("python Image_extractor/get_scene_objects.py " + dataset + " " + scene)
 			for room in os.listdir(os.path.join(scene_path,"rooms")):
 
 				print("python get_raw_data.py " + dataset + " " + scene + " " + room)
-				os.system("python get_raw_data.py " + dataset + " " + scene + " " + room)
+				os.system("python Image_extractor/get_raw_data.py " + dataset + " " + scene + " " + room)
 
 
